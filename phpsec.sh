8be3b83301b62f910e314962bd0770ff653de70d
Yi <84hero@users.noreply.github.com>
2018-03-20 11:09:32 +0800

    Update phpsec.sh

--- phpsec.sh
+++ phpsec.sh
@@ -1,3 +1,14 @@
 #!/usr/bin/env bash
 #php syntax check
-find `pwd` -name \*.php -exec php -l "{}" \; |grep -v "No syntax errors"
+
+file=$1
+
+if  [ ! -n "$file" ] ;then
+    file=`pwd`
+fi
+
+if  [ ! -d "$file" ] ;then
+    php -l $file \; |grep -v "No syntax errors" |grep "PHP Parse error"
+else
+	find $file -name \*.php -exec php -l "{}" \; |grep -v "No syntax errors"
+fi
